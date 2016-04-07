07fb68125930b03514d0c39521cf7fab17ba2d46
Steffen Gorenflo <SteffenGorenflo@gmail.com>
2016-04-07 08:37:49 +0200

    add all queries

--- exercise1/part2.js
+++ exercise1/part2.js
@@ -12,10 +12,27 @@ ain = db.studiengang.findOne({kurzel: 'AIN'});
 db.vorlesungen
     .find(
         {
-            'studiengang._id': ain._id,
+            'studiengang.$id': ain._id,
             sws: {$lt: 5}
         })
     .sort({name: 1})
     .forEach(function (u) {
         print(u.name)
+    });
+
+
+db.vorlesungen
+    .find({$where: 'this.sws > this.ects'})
+    .forEach(function (u) {
+        print(u.name)
+    });
+
+db.vorlesungen.aggregate([
+        {$match: {'studiengang.$id': ain._id}},
+        {$group: {_id: '$dozent', sws: {$sum: '$sws'}}},
+        {$sort: {sws: -1}},
+        //  {$limit: 1}
+    ])
+    .forEach(function (u) {
+        print(u._id +' --> ' + u.sws + ' SWS')
     });
